910d4ec0b28902b7184ceb44ce3391a339043fc5
pya-h <p4ya.py@gmail.com>
2023-12-14 03:07:17 +0330

    editing routed for gh-pages

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: 'users', component: UsersComponent },
   { path: 'users/:id', component: EditUserComponent },
 
-  // { path: '', redirectTo: '/new', pathMatch: 'full' },
+  { path: '', redirectTo: '/new', pathMatch: 'full' },
   // { path: 'ng-forms-app', redirectTo: '/ng-forms-app/new', pathMatch: 'full' },
 
 ];
